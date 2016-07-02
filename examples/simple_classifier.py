f098b063cf512c0f771f73af3c8a5f862b73bef1
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2016-07-02 17:43:15 +0200

    Add example of scikit classification

---
+++ examples/simple_classifier.py
@@ -0,0 +1,36 @@
+import numpy as np
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn import cross_validation
+
+from rootpy.plotting import Hist2D, Hist3D
+from rootpy.io import root_open
+from root_numpy import root2array
+
+
+
+def fit(filename1, treename1, filename2, treename2, inputsname):
+    # Reading inputs from ROOT tree
+    data1 = root2array(filename1, treename=treename1, branches=inputsname)
+    data2 = root2array(filename2, treename=treename2, branches=inputsname)
+    data1 = data1.view((np.float64, len(data1.dtype.names)))
+    data2 = data2.view((np.float64, len(data2.dtype.names)))
+    # Creating target class arrays
+    class1 = np.zeros((data1.shape[0],))
+    class2 = np.ones((data2.shape[0],))
+    # Merging datasets
+    inputs = np.concatenate((data1, data2))
+    classes = np.concatenate((class1, class2))
+    # Split events in a training sample and a test sample (60% used for training)
+    inputs_train, inputs_test, targets_train, targets_test = cross_validation.train_test_split(inputs, classes, test_size=0.4, random_state=0)
+    # Fit and test classifier (BDT with gradient boosting)
+    # Default training parameters are used
+    clf = GradientBoostingClassifier()
+    clf.fit(inputs_train, targets_train)
+    print 'Accuracy on test sample:', clf.score(inputs_test, targets_test)
+
+
+if __name__=='__main__':
+    file_w = '/afs/cern.ch/work/s/steggema/public/mt/070416/TauMuSVFitMC/WJetsToLNu_LO/H2TauTauTreeProducerTauMu/tree.root'
+    file_qcd = '/afs/cern.ch/work/s/steggema/public/mt/070416/TauMuSVFitMC/QCD_Mu15/H2TauTauTreeProducerTauMu/tree.root'
+    inputs = ['mt','l2_decayMode']
+    fit(file_w, 'tree', file_qcd, 'tree', inputs)
